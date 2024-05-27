1fbfbc2f4c6f0d4f53e72f6203a98bb0219d2985
jacky4631 <121659050@qq.com>
2024-05-27 15:07:58 +0800

    实现用户反馈

---
+++ src/views/userFeedback/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <div v-if="crud.props.searchToggle">
+        <!-- 搜索 -->
+        <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
+          <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+        </el-select>
+        <rrOperation :crud="crud" />
+      </div>
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="用户id" prop="uid">
+            <el-input v-model="form.uid" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="反馈内容" prop="feedback">
+            <el-input :rows="3" v-model="form.feedback" type="textarea" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column v-if="columns.visible('id')" prop="id" label="id" />
+        <el-table-column v-if="columns.visible('uid')" prop="uid" label="用户id" />
+        <el-table-column v-if="columns.visible('feedback')" prop="feedback" label="反馈内容" />
+        <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="添加时间">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-if="columns.visible('updateTime')" prop="updateTime" label="更新时间">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.updateTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-permission="['admin','mwUserFeedback:edit','mwUserFeedback:del']" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudMwUserFeedback from '@/api/mwUserFeedback'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+import MaterialList from "@/components/material";
+
+// crud交由presenter持有
+const defaultCrud = CRUD({ title: 'userFeedback', url: 'api/mwUserFeedback', sort: 'id,desc', crudMethod: { ...crudMwUserFeedback }})
+const defaultForm = { id: null, uid: null, feedback: null, createTime: null, updateTime: null, isDel: null }
+export default {
+  name: 'MwUserFeedback',
+  components: { pagination, crudOperation, rrOperation, udOperation ,MaterialList},
+  mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
+  data() {
+    return {
+
+      permission: {
+        add: ['admin', 'mwUserFeedback:add'],
+        edit: ['admin', 'mwUserFeedback:edit'],
+        del: ['admin', 'mwUserFeedback:del']
+      },
+      rules: {
+        uid: [
+          { required: true, message: '用户id不能为空', trigger: 'blur' }
+        ],
+        feedback: [
+          { required: true, message: '反馈内容不能为空', trigger: 'blur' }
+        ]
+      },
+      queryTypeOptions: [
+        { key: 'uid', display_name: '用户id' }
+      ]
+    }
+  },
+  watch: {
+  },
+  methods: {
+    // 获取数据前设置好接口地址
+    [CRUD.HOOK.beforeRefresh]() {
+      const query = this.query
+      if (query.type && query.value) {
+        this.crud.params[query.type] = query.value
+      }else{
+        delete this.crud.params.uid
+      }
+      return true
+    }, // 新增与编辑前做的操作
+    [CRUD.HOOK.afterToCU](crud, form) {
+    },
+  }
+}
+
+
+
+</script>
+
+<style scoped>
+  .table-img {
+    display: inline-block;
+    text-align: center;
+    background: #ccc;
+    color: #fff;
+    white-space: nowrap;
+    position: relative;
+    overflow: hidden;
+    vertical-align: middle;
+    width: 32px;
+    height: 32px;
+    line-height: 32px;
+  }
+</style>
